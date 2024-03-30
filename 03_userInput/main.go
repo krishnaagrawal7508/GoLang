9854d8c07bce485430a1305bd7b5572caaf52cdc
krishna agrawal <agrawalkrishna522@gmail.com>
2024-03-31 03:09:07 +0530

    input user

--- 03_userInput/main.go
+++ 03_userInput/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
-	
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "Welcom to user input"
+	fmt.Println(welcome)
+
+	//comma okay syntax
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter a rating: ")
+
+	// comma ok || error ok
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating,", input);
+	fmt.Printf("Type of this rating is %T", input);
 }
