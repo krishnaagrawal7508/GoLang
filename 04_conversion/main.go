8d6e5a7c8f13b3303abc33e8db8ce916ddfe046a
krishna agrawal <agrawalkrishna522@gmail.com>
2024-03-31 03:13:18 +0530

    comment

--- 04_conversion/main.go
+++ 04_conversion/main.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-	welcome := "Welcom to user input"
-	fmt.Println(welcome)
+	fmt.Println("welcome to pizza app")
+	fmt.Println("Please rate our pizza from 1 to 5")
 
 	//comma okay syntax
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter a rating: ")
 
 	// comma ok || error ok
-	input, _ := reader.ReadString('\n') //it means to read until enter is pressed
+	input, _ := reader.ReadString('\n')
 	fmt.Println("Thanks for rating,", input);
 	fmt.Printf("Type of this rating is %T", input);
 }
