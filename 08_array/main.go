893ce8c25799ce386c31924d3cf48bb50efbb87f
krishna agrawal <agrawalkrishna522@gmail.com>
2024-04-01 21:58:56 +0530

    arrays

---
+++ 08_array/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+//in golang array is not used often
+//there is another data type which is used instead
+
+func main() {
+	fmt.Println("Welcome to Array class")
+
+	var fruitList [4]string
+
+	fruitList[0] = "Apple"
+	fruitList[1] = "Tomata"
+	//fruitList[2] is not given any value, so it given " "
+	fruitList[3] = "Peach"
+
+	fmt.Println("Our fruit-list: ", fruitList)
+	fmt.Println("Length of fruit-list: ", len(fruitList))
+
+	
+	var vegList = [4]string {"potato","beans","mushroom"}
+	fmt.Println("Our veg-List: ", vegList)
+	fmt.Println("Length of veg-List: ", len(vegList))
+}
