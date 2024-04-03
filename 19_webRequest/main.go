a213f02b596bc4f96693f676fe2af78917e098aa
krishna agrawal <agrawalkrishna522@gmail.com>
2024-04-03 21:25:09 +0530

    web request get html

---
+++ 19_webRequest/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const url = "http://txti.es/"
+
+func main() {
+	fmt.Println("<---Welcome to Web Requests Class--->")
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response type %T\n\n\n", response)
+
+	//caller's responsibility to close connection
+	defer response.Body.Close()
+	
+	databyte, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(databyte))
+
+}
